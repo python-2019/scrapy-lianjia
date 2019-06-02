3dfecfb3ff263f4be451db0598c319bdac1f80da
shanmu <854731306@QQ.com>
2019-06-02 17:17:48 +0800

    .

--- lianjia/pipelines.py
+++ lianjia/pipelines.py
@@ -15,12 +15,13 @@ class LianjiaPipeline(object):
         file_path = settings.get("FILE_PATH")
         self.file = open(file_path, 'w', newline='', encoding='utf-8')
         self.csv_writer = csv.writer(self.file)
-        headers = ['楼盘名称', '位置', '基础信息', '户型', '面积', '单价', '总价', '优势', '开售时间', '详情地址']
+        headers = ['楼盘名称', '位置', '单价', '总价', '基础信息', '户型', '面积', '优势', '开售时间', '详情地址']
         self.csv_writer.writerow(headers)
 
     def process_item(self, item, spider):
-        row = [item['name'], item['addr'], item['base_info'], item['apartment'], item['area'], item['unit_price'],
-               item['total_price'], item['advantage'], item['start_time'], item['href']]
+        row = [item['name'], item['addr'], item['unit_price'], item['total_price'], item['base_info'],
+               item['apartment'], item['area'],
+               item['advantage'], item['start_time'], item['href']]
         self.csv_writer.writerow(row)
         print(row)
         return item
